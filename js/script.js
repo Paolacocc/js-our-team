05695720a405a44f497f7f6cc1b271e2e97e7edd
Paolacocc <coccettipaola@gmail.com>
2023-03-23 10:00:55 +0100

    improved img

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@ const teamMembersArray = [
     {
        name: "Wayne Barnett",
        ruolo: "Founder & CEO",
-       foto: "img/wayne-barnett-founder-ceo.jpg"
+       foto: "wayne-barnett-founder-ceo.jpg"
     },
     {
        name: "Angela Caroll",
        ruolo: "Chief Editor",
-       foto: "img/angela-caroll-chief-editor.jpg"
+       foto: "angela-caroll-chief-editor.jpg"
     },
     {
        name: "Walter Gordon",
        ruolo: "Office Manager",
-       foto: "img/walter-gordon-office-manager.jpg"
+       foto: "walter-gordon-office-manager.jpg"
     },
     {
        name: "Angela Lopez",
        ruolo: "Social Media Manager",
-       foto: "img/angela-lopez-social-media-manager.jpg"
+       foto: "angela-lopez-social-media-manager.jpg"
     },
     {
        name: "Scott Estrada",
        ruolo: "Developer",
-       foto: "img/scott-estrada-developer.jpg"
+       foto: "scott-estrada-developer.jpg"
     },
     {
        name: "Barbara Ramos",
        ruolo: "Graphic Designer",
-       foto: "img/barbara-ramos-graphic-designer.jpg"
+       foto: "barbara-ramos-graphic-designer.jpg"
     }
 ];
 let list = "";
@@ -38,7 +38,7 @@ for (let i = 0; i < teamMembersArray.length; i++) {
 
     list += `<div class="col">
                 <div class="card">
-                    <img src="${objectArray.foto}" class="card-img-top" alt="...">
+                    <img src="img/${objectArray.foto}" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h4 class="card-title">${objectArray.name}</h4>
                     <h5 class="card-title">${objectArray.ruolo}</h5>
